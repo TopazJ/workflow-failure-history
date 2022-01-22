1db5db4e85f69f8613b0966421eade81c4ace9b3
TopazJ <shamez.meghji1@ucalgary.ca>
2022-01-22 01:25:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Workflow Failure History
+
+This action provides the number of failures since the last success for a specific workflow.
+
+## Inputs
+
+## `token`
+
+**Required** The token with authorization to access previous runs for the desired workflow.
+
+## `owner`
+
+**Required** The GitHub username of the owner of the repository containing the workflow.
+
+## `repo`
+
+**Required** The name of the repository containing the workflow.
+
+## `workflow_id`
+
+**Required** The ID or filename of the workflow.
+
+## Outputs
+
+## `failures`
+
+The number of failures since the last success for all the workflow runs.
+
+## Example usage
+
+uses: TopazJ/workflow-failure-history@v1.0
+with:
+  token: ${{ secrets.GITHUB_TOKEN }}
+  owner: TopazJ
+  repo: integration-action-test
+  workflow_id: e2e_test.yml
---
+++ index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const { Octokit } = require("@octokit/core");
+
+try {
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const repo = core.getInput('repo');
+    const workflow_id = core.getInput('workflow_id');
+
+    const octokit = new Octokit({ auth: token });
+
+    let parsed_runs = 0;
+    let success = false;
+    let failures = 0;
+    let page = 1;
+
+    do {
+        let { data } = await octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs?page='+page, {
+            owner: owner,
+            repo: repo,
+            workflow_id: workflow_id,
+        });
+        page++;
+        
+        for (let i = 0; i < data.workflow_runs.length; i++)
+        {
+            parsed_runs++;
+            if (data.workflow_runs[i].conclusion === "success")
+            {
+                success = true;
+                break;
+            }
+            if (data.workflow_runs[i].conclusion === "failure")
+            {
+                failures++;
+            }
+        }
+        if (parsed_runs === data.total_count)
+        {
+            success = true;
+        }
+    } while (!success);
+    console.log("Discovered " + failures + " failures since the last success.");
+    core.setOutput("failures", failures);
+} catch (error) {
+    core.setFailed(error.message);
+}
